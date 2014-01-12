2677c1cd3c32b8fd7e0919d9bb56d40738203d81
Frank Robin Danielsen <frdanielsen@gmail.com>
2014-01-12 10:57:00 +0100

    Updated REST API to bind to new file structure - all information is contained in one master file

--- src/SpeakerRESTAPI.py
+++ src/SpeakerRESTAPI.py
@@ -8,7 +8,7 @@ import json
 import re
 
 datafile = open('../resources/fixtures.txt')
-fixtures = json.load(datafile)
+all_data = json.load(datafile)
 
 urls = (
     '/', 'index',
@@ -19,6 +19,32 @@ urls = (
 
 
 VALID_KEY = re.compile('[a-zA-Z0-9_-]{1,255}')
+
+''' A section in the fixtures.txt file is a uniform section of content:
+    Currently implemented sections are:
+    - Teams: List of all teams and teamID. TeamID is used to get name from Speaker.no site.
+    - Fixtures: List of fixtures per team.
+    - N/A
+    
+'''
+def find_section(id, obj):
+    results = []
+
+    def _find_values(id, obj):
+        try:
+            for record in obj:
+                if record[0] == id:
+                    results.append(record[1])
+                    
+        except AttributeError:
+            pass
+
+    
+    if not isinstance(obj, basestring):
+        _find_values(id, obj)
+    
+    return results
+
 def is_valid_key(key):
     """Checks to see if the parameter follows the allow pattern of
     keys.
@@ -44,12 +70,13 @@ class list_teams:
     def GET(self):
         #for i in fixtures:
         #    print i[1]
-        return render.innebandy(fixtures)
+
+        return render.innebandy(web.teams[0])
             
 class list_fixtures:
     def GET(self, name):
         name_set = False
-        for i in fixtures:
+        for i in web.fixtures[0]:
             if i[0]==name:
                 if not name_set:
                     name_set = True
@@ -68,7 +95,11 @@ class index:
         return render.index()
 
 if __name__ == '__main__':
+    teams = find_section('Teams', all_data)
+    fixtures = find_section('Fixtures', all_data)
     
+    web.teams = teams
+    web.fixtures = fixtures
     app = web.application(urls, globals())
     app.run()
     
